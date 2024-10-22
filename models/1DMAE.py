019e06f283570c4e2b17f8ce89e900abdc674b6b
IParraMartin <82852463+IParraMartin@users.noreply.github.com>
2024-10-21 22:51:34 -0700

    Refactor Patchify module in 1DMAE.py and add rearrangement step

--- models/1DMAE.py
+++ models/1DMAE.py
@@ -1,4 +1,5 @@
 import torch
+import torchaudio
 import torch.nn as nn
 import matplotlib.pyplot as plt
 import einops
@@ -65,6 +66,10 @@ class Encoder(nn.Module):
 
 class Decoder(nn.Module):
 
+    """
+    Learn to reconstruct using the representations of the encoder
+    """
+
     def __init__(self, n_layers, d_model, n_heads, h_dims, dropout, activation, batch_first):
         super().__init__()
         self.decoder_layer = nn.TransformerDecoderLayer(
@@ -88,20 +93,30 @@ class Decoder(nn.Module):
 
 if __name__ == "__main__":
 
-    memory_in = torch.randn(16, 1, 512)
-    decoder_in = torch.randn(16, 1, 512)
-    enc = Encoder(n_layers=6, d_model=512, n_heads=8, h_dims=2048, dropout=0.2, activation='gelu', batch_first=True)
-    dec = Decoder(n_layers=6, d_model=512, n_heads=8, h_dims=2048, dropout=0.2, activation='gelu', batch_first=True)
+    # memory_in = torch.randn(16, 1, 512)
+    # decoder_in = torch.randn(16, 1, 512)
+    # enc = Encoder(n_layers=6, d_model=512, n_heads=8, h_dims=2048, dropout=0.2, activation='gelu', batch_first=True)
+    # dec = Decoder(n_layers=6, d_model=512, n_heads=8, h_dims=2048, dropout=0.2, activation='gelu', batch_first=True)
 
-    memory = enc(memory_in)
-    out = dec(decoder_in, memory)
-    print(out.shape)
+    # memory = enc(memory_in)
+    # out = dec(decoder_in, memory)
+    # print(out.shape)
 
-    # patch = Patchify(512, 25, 1920, 0.2, 1)
-    # dummy_in = torch.randn(4, 1, 48000)
-    # x = patch(dummy_in)
-    # print(x[0])
-    # print(x.shape)
+    patch = Patchify(512, 50, 960, 0.2, 1)
+    audio, sr = torchaudio.load('/Users/inigoparra/Desktop/Datasets/speech/7059-77900-0003.flac')
+    signal = audio[:, 0:48000].unsqueeze(1)
+    print(signal.shape)
+
+    x = patch(signal)
+    print(x.shape)
+
+    def visualize_embeddings(tensor):
+        plt.imshow(tensor[0].squeeze().detach().numpy(), cmap='magma', origin='lower')
+        plt.colorbar()
+        plt.xlabel('d_model')
+        plt.ylabel('Patch Embeddings')
+        plt.tight_layout()
+        plt.show()
 
 
 
